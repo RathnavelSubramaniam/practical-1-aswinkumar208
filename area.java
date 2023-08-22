0f64aa3d92e901d6eade957abb40d0d8b52cd03e
aswinkumar208 <142867822+aswinkumar208@users.noreply.github.com>
2023-08-22 05:33:12 +0000

    complete the java circle

---
+++ area.java
@@ -0,0 +1,17 @@
+class area{
+    public static void main (String args[]){
+    double pi=3.14159;    
+    double r=10;
+    double a=pi*r*r;
+    System.out.println("the area of the circle is"+a);
+    }
+}
+
+        
+
+        
+
+    
+    
+
+    
